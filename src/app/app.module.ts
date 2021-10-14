c92618e11a1d6977f95d87d538a721283a088c47
Максим Соколов <maks201322@yandex.ru>
2021-10-15 00:22:56 +0500

    Сделан интерфейс

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 import {CardComponent} from "./card/card.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,6 +11,7 @@ import {CardComponent} from "./card/card.component";
   ],
   imports: [
     BrowserModule,
+    NgbModule,
 
   ],
   providers: [],
